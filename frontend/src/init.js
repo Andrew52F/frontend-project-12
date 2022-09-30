1d6d0f047e643bf8e2f0332ce96b45b80233add3
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2022-09-30 18:39:04 +0300

    fix .env

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import i18n from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import AuthProvider from './components/providers/AuthProvider';
 import ChatApiProvider from './components/providers/ChatApiProvider';
 import resources from './locales/index';
@@ -9,7 +9,6 @@ import store from './slices/index';
 import initSocket from './websocket';
 import Rollbar from 'rollbar';
 import { Provider as RollbarProvider } from '@rollbar/react';
-
 
 
 const Providers = ({children}) => {
@@ -23,8 +22,8 @@ const Providers = ({children}) => {
   const api = initSocket();
 
   const rollbarConfig = {
-    accessToken: '9564894f8c69445b9ec982a1a26e88c7',
-    environment: 'production',
+    accessToken: process.env.ROLLBAR_TOKEN,
+    environment: process.env.NODE_ENV,
     captureUncaught: true,
     captureUnhandledRejections: true,
     payload: {
@@ -32,18 +31,17 @@ const Providers = ({children}) => {
     },
   };
   
-  const rollbar = new Rollbar(rollbarConfig);
 
   return (
-    <RollbarProvider instance={rollbar} >
+    <RollbarProvider config={rollbarConfig} >
       <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
+        <StoreProvider store={store}>
           <AuthProvider>
             <ChatApiProvider api={api}>
               { children }
             </ChatApiProvider>
           </AuthProvider>
-        </Provider>
+        </StoreProvider>
       </I18nextProvider>
     </RollbarProvider>
   )
